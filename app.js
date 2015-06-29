b412fad6b5dbebaba6b409d137dcd9ef2570c7af
fransoni <fransoni@gmail.com>
2015-06-29 21:06:41 +0200

    Añadidos Modulo 6

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 
-var routes = require('./routes/index');
 //var users = require('./routes/users');
 
 var routes = require('./routes/index');
