18caf60dce0feb68d3c498e3db9d9d68cc2fe1ae
fransoni <fransoni@gmail.com>
2015-07-03 17:26:49 +0200

    Añadidos Modulo 6 II

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -11,3 +11,8 @@ exports.answer = function(req, res){
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
 };
+
+// GET quizes/question
+exports.author = function(req, res) {
+	res.render('author', {autor: 'Francisco J.M.P.'});	
+};
